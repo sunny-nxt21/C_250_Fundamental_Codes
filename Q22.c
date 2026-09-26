f1235742b2e6f2a2004f96105fc311dd6ef8f02b
Ahaduzzaman Sunny <ajsunny977@gmail.com>
2026-09-27 03:32:08 +0600

    Update Q22

--- Q22.c
+++ Q22.c
@@ -1,25 +1,26 @@
 // Write a C program to convert days into years, months and days.
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
 int main()
 {
-    float days;
-    float years;
-    float months;
-    float remaining_days;
-    
+    int days;
+    int years;
+    int months;
+    int remaining_days;
+
     printf("days = ");
     scanf("%d", &days);
 
     years = days / 365;
-    months = fmod(days / 30);
-    remaining_days = fmod(months / 30);
+    remaining_days = days % 365;
 
-    printf("years = %f", years);
-    printf("months = %d", months);
+    months = remaining_days / 30;
+    remaining_days = remaining_days % 30;
+
+    printf("years = %d\n", years);
+    printf("months = %d\n", months);
     printf("remaining_days = %d", remaining_days);
-    
+
     return 0;
 }
